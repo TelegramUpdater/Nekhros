0d39e4cb69580d044b14d52ec1d9d861cb368f11
immmdreza <ir310022@gmail.com>
2022-03-14 18:45:34 +0330

    Fixes on commands

--- Nekhros/UpdateHandlers/Messages/About.cs
+++ Nekhros/UpdateHandlers/Messages/About.cs
@@ -1,11 +1,16 @@
 ﻿using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 using TelegramUpdater.FilterAttributes.Attributes;
 using TelegramUpdater.UpdateContainer;
 using TelegramUpdater.UpdateHandlers.Scoped.ReadyToUse;
 
 namespace Nekhros.UpdateHandlers.Messages
 {
-    [Command("about", "Read more about me.", 1), Private]
+    [
+        Command("about", "Read more about me.", 1,
+            botCommandScopeType: BotCommandScopeType.AllPrivateChats),
+        Private
+    ]
     internal sealed class About : MessageHandler
     {
         protected override async Task HandleAsync(IContainer<Message> cntr)
